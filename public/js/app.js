53fa3af73d5610ea2db9f7f15303004f0df6cc03
Randy Bradshaw <randy.bradshaw@gmail.com>
2020-05-19 14:02:50 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,6 @@ const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error){
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -23,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
 
     //console.log(location)
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error){
                 messageOne.textContent = data.error
